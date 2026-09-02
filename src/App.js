911f084aa071b744fb8878950046d278d97d4948
ghuglu <surbhibaraik6@gmail.com>
2026-09-02 21:15:05 +0530

    fix conflict

--- src/App.js
+++ src/App.js
@@ -3,28 +3,17 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("home");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Read reactjs 1234
-        </a>
-      </header>
+      {page === "home" && <Home setPage={setPage} />}
+      {page === "login" && <Login setPage={setPage} />}
+      {page === "register" && <Register setPage={setPage} />}
+      {page === "dashboard" && <Dashboard setPage={setPage} />}
     </div>
   );
 }
-
 export default App;
